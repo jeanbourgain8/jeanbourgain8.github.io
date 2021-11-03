2bb3f06649915a52657f1429bb6398662899a066
jeanbourgain8 <jeanbourgain8@gmail.com>
2021-11-03 13:00:40 +0530

    Nikola auto commit.
    Nikola version: 8.1.3

--- cache/.mako.tmp/tagindex.tmpl.py
+++ cache/.mako.tmp/tagindex.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1635911726.557512
+_modified_time = 1635924637.9994278
 _enable_loop = True
 _template_filename = 'themes/custom/templates/tagindex.tmpl'
 _template_uri = 'tagindex.tmpl'
@@ -33,16 +33,16 @@ def render_body(context,**pageargs):
         _import_ns = {}
         _mako_get_namespace(context, 'feeds_translations')._populate(_import_ns, ['*'])
         kind = _import_ns.get('kind', context.get('kind', UNDEFINED))
-        subcategories = _import_ns.get('subcategories', context.get('subcategories', UNDEFINED))
         description = _import_ns.get('description', context.get('description', UNDEFINED))
+        subcategories = _import_ns.get('subcategories', context.get('subcategories', UNDEFINED))
         parent = _import_ns.get('parent', context.get('parent', UNDEFINED))
+        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
+        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
         feeds_translations = _mako_get_namespace(context, 'feeds_translations')
         def extra_head():
             return render_extra_head(context._locals(__M_locals))
         def content_header():
             return render_content_header(context._locals(__M_locals))
-        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
-        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer('\n\n')
@@ -67,13 +67,13 @@ def render_content_header(context,**pageargs):
         _import_ns = {}
         _mako_get_namespace(context, 'feeds_translations')._populate(_import_ns, ['*'])
         kind = _import_ns.get('kind', context.get('kind', UNDEFINED))
-        subcategories = _import_ns.get('subcategories', context.get('subcategories', UNDEFINED))
         description = _import_ns.get('description', context.get('description', UNDEFINED))
+        subcategories = _import_ns.get('subcategories', context.get('subcategories', UNDEFINED))
+        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
+        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
         feeds_translations = _mako_get_namespace(context, 'feeds_translations')
         def content_header():
             return render_content_header(context)
-        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
-        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
         __M_writer = context.writer()
         __M_writer('\n    <header>\n')
         if description:
@@ -108,10 +108,10 @@ def render_extra_head(context,**pageargs):
         _mako_get_namespace(context, 'feeds_translations')._populate(_import_ns, ['*'])
         kind = _import_ns.get('kind', context.get('kind', UNDEFINED))
         parent = _import_ns.get('parent', context.get('parent', UNDEFINED))
+        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
         feeds_translations = _mako_get_namespace(context, 'feeds_translations')
         def extra_head():
             return render_extra_head(context)
-        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
         __M_writer = context.writer()
         __M_writer('\n    ')
         __M_writer(str(parent.extra_head()))
---
+++ pages/subscribe.md
@@ -0,0 +1,34 @@
+.. title: Substack Blogs
+.. slug: subscribe
+.. hidetitle: true
+.. template: page.tmpl
+.. date: 2021-11-03 09:04:38 UTC+05:30
+
+
+<div class="row">
+	<div class="col-md-6">
+		<iframe src="https://scientificsociety.substack.com/embed" max-width="480" height="320" frameborder="0" scrolling="no" class="centred"></iframe>
+		<p> 1) In <a href="https://randomdots.substack.com/"><strong>Science & Society</strong></a>, I write posts on topics - <strong>Public Policy, Economics, General Science, Psychology, Book Reviews/Summaries etc.</strong>, with my posts, I’ll bring to you a compendium of latest developments, thought provoking concepts, arguments & shall aid in connecting them.</p>
+	</div>
+	<div class="col-md-6">
+		<iframe src="https://theorywalks.substack.com/embed" max-width="480" height="320" frameborder="0" scrolling="no" class="centred"></iframe>	
+		<p>4) In <a href="https://upscdots.substack.com/"><strong>Connecting Collector</strong></a>, I post my daily <strong>answer-writings</strong> as an account of my answer-writings to track my progress, I can see <a href="https://upscdots.substack.com/"><strong>Connecting Collector</strong></a> becoming a comprehensive resource on answer-writing for <strong>UPSC  Aspirants </strong> & also as a good collection to the General Audience, who are genuinely interested in knowing the contours of <strong>development of India</strong>, as a nation & society.</p>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-6">
+		<iframe src="https://talkingbook.substack.com/embed" max-width="480" height="320" frameborder="0" scrolling="no" class="centred"></iframe>
+		<p> 3) In <a href="https://randomdots.substack.com/"><strong>Science & Society</strong></a>, I write posts on topics - <strong>Public Policy, Economics, General Science, Psychology, Book Reviews/Summaries etc.</strong>, with my posts, I’ll bring to you a compendium of latest developments, thought provoking concepts, arguments & shall aid in connecting them.</p>
+	</div>
+	<div class="col-md-6">
+		<iframe src="https://codingmonk.substack.com/embed" max-width="480" height="320" frameborder="0" scrolling="no" class="centred"></iframe>
+		<p> 5) In <a href="https://randomdots.substack.com/"><strong>Science & Society</strong></a>, I write posts on topics - <strong>Public Policy, Economics, General Science, Psychology, Book Reviews/Summaries etc.</strong>, with my posts, I’ll bring to you a compendium of latest developments, thought provoking concepts, arguments & shall aid in connecting them.</p>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-12">
+		<iframe src="https://collectorconnects.substack.com/embed" max-width="480" height="320" frameborder="0" scrolling="no" class="centred"></iframe>
+		<p>2) In <a href="https://upscdots.substack.com/"><strong>Connecting Collector</strong></a>, I post my daily <strong>answer-writings</strong> as an account of my answer-writings to track my progress, I can see <a href="https://upscdots.substack.com/"><strong>Connecting Collector</strong></a> becoming a comprehensive resource on answer-writing for <strong>UPSC  Aspirants </strong> & also as a good collection to the General Audience, who are genuinely interested in knowing the contours of <strong>development of India</strong>, as a nation & society.</p>			
+	</div>
+</div>
+
