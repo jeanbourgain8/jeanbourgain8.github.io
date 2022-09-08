6f4aff5112e6565a4b267214d0e85a8356f1afcd
Jean Bourgain <jeanbourgain8@gmail.com>
2022-09-08 18:15:18 +0530

    Nikola auto commit.
    Nikola version: 8.2.3

--- Tasks.md
+++ Tasks.md
@@ -8,6 +8,8 @@ kanban-plugin: basic
 
 - [ ] Writing Articles to [IGC](https://www.theigc.org/blog/)
 - [ ] Writing Articles to Hindu, IE & EPW
+- [ ] Check the [hugo-xmin](https://github.com/yihui/hugo-xmin/) theme
+
 
 
 
--- cache/.mako.tmp/post_helper.tmpl.py
+++ cache/.mako.tmp/post_helper.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1661872435.6995378
+_modified_time = 1662641112.867554
 _enable_loop = True
 _template_filename = 'themes/custom/templates/post_helper.tmpl'
 _template_uri = 'post_helper.tmpl'
@@ -45,8 +45,8 @@ def render_meta_translations(context,post):
     try:
         lang = context.get('lang', UNDEFINED)
         translations = context.get('translations', UNDEFINED)
-        sorted = context.get('sorted', UNDEFINED)
         len = context.get('len', UNDEFINED)
+        sorted = context.get('sorted', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
         if len(translations) > 1:
@@ -120,9 +120,9 @@ def render_open_graph_metadata(context,post):
     try:
         abs_link = context.get('abs_link', UNDEFINED)
         url_replacer = context.get('url_replacer', UNDEFINED)
-        lang = context.get('lang', UNDEFINED)
-        blog_title = context.get('blog_title', UNDEFINED)
         permalink = context.get('permalink', UNDEFINED)
+        blog_title = context.get('blog_title', UNDEFINED)
+        lang = context.get('lang', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n<meta property="og:site_name" content="')
         __M_writer(filters.html_escape(str(blog_title)))
