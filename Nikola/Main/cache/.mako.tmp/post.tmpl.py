8e0dce4a1a766aed7c05d01012cca263d3559514
jeanbourgain8 <jeanbourgain8@gmail.com>
2023-07-30 16:53:37 +0530

    Nikola auto commit.
    Nikola version: 8.2.3

--- Nikola/Main/cache/.mako.tmp/post.tmpl.py
+++ Nikola/Main/cache/.mako.tmp/post.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1690713694.7008255
+_modified_time = 1690716211.019606
 _enable_loop = True
 _template_filename = '/home/jeanbourgain8/miniconda3/envs/nikola/lib/python3.10/site-packages/nikola/data/themes/bootstrap4/templates/post.tmpl'
 _template_uri = 'post.tmpl'
@@ -42,23 +42,23 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
+        def content():
+            return render_content(context._locals(__M_locals))
+        comments = _mako_get_namespace(context, 'comments')
+        parent = context.get('parent', UNDEFINED)
+        pheader = _mako_get_namespace(context, 'pheader')
+        math = _mako_get_namespace(context, 'math')
+        ui = _mako_get_namespace(context, 'ui')
+        helper = _mako_get_namespace(context, 'helper')
+        messages = context.get('messages', UNDEFINED)
+        def sourcelink():
+            return render_sourcelink(context._locals(__M_locals))
         def extra_head():
             return render_extra_head(context._locals(__M_locals))
         show_sourcelink = context.get('show_sourcelink', UNDEFINED)
-        parent = context.get('parent', UNDEFINED)
-        helper = _mako_get_namespace(context, 'helper')
-        messages = context.get('messages', UNDEFINED)
-        smartjoin = context.get('smartjoin', UNDEFINED)
-        post = context.get('post', UNDEFINED)
-        def sourcelink():
-            return render_sourcelink(context._locals(__M_locals))
-        comments = _mako_get_namespace(context, 'comments')
         site_has_comments = context.get('site_has_comments', UNDEFINED)
-        def content():
-            return render_content(context._locals(__M_locals))
-        math = _mako_get_namespace(context, 'math')
-        ui = _mako_get_namespace(context, 'ui')
-        pheader = _mako_get_namespace(context, 'pheader')
+        post = context.get('post', UNDEFINED)
+        smartjoin = context.get('smartjoin', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer('\n')
@@ -90,12 +90,12 @@ def render_extra_head(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         parent = context.get('parent', UNDEFINED)
-        helper = _mako_get_namespace(context, 'helper')
-        smartjoin = context.get('smartjoin', UNDEFINED)
-        post = context.get('post', UNDEFINED)
         def extra_head():
             return render_extra_head(context)
         math = _mako_get_namespace(context, 'math')
+        helper = _mako_get_namespace(context, 'helper')
+        post = context.get('post', UNDEFINED)
+        smartjoin = context.get('smartjoin', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n    ')
         __M_writer(str(parent.extra_head()))
@@ -138,15 +138,15 @@ def render_extra_head(context,**pageargs):
 def render_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        helper = _mako_get_namespace(context, 'helper')
-        messages = context.get('messages', UNDEFINED)
-        post = context.get('post', UNDEFINED)
-        comments = _mako_get_namespace(context, 'comments')
-        site_has_comments = context.get('site_has_comments', UNDEFINED)
         def content():
             return render_content(context)
-        math = _mako_get_namespace(context, 'math')
+        comments = _mako_get_namespace(context, 'comments')
         pheader = _mako_get_namespace(context, 'pheader')
+        math = _mako_get_namespace(context, 'math')
+        helper = _mako_get_namespace(context, 'helper')
+        messages = context.get('messages', UNDEFINED)
+        site_has_comments = context.get('site_has_comments', UNDEFINED)
+        post = context.get('post', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n<article class="post-')
         __M_writer(str(post.meta('type')))
@@ -178,11 +178,11 @@ def render_content(context,**pageargs):
 def render_sourcelink(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
+        show_sourcelink = context.get('show_sourcelink', UNDEFINED)
         def sourcelink():
             return render_sourcelink(context)
-        show_sourcelink = context.get('show_sourcelink', UNDEFINED)
-        ui = _mako_get_namespace(context, 'ui')
         post = context.get('post', UNDEFINED)
+        ui = _mako_get_namespace(context, 'ui')
         __M_writer = context.writer()
         __M_writer('\n')
         if show_sourcelink:
--- Nikola/Main/posts/Substack/Page/Goodreads/The Case of The Sulky Girl.md
+++ Nikola/Main/posts/Substack/Page/Goodreads/The Case of The Sulky Girl.md
@@ -24,11 +24,12 @@ For this book, I was hanging between 3 & 4 stars which one to give, three being 
     
 The character setup itself should hint some of the story arc to someone who has developed a little caution eye for the workings of the society. Yet, it consists of twists and turns that's expected from this series. 
 
-Fran Celane, a happy go cranky lady, whose inheritence transfer was put into the hands of her uncle Norton approaches Mr. Mason for help. Mason accepts Fran as his client and jumps into math of the unfortunate event took place which has pushed Fran under the bus. She's been made a prime suspect for a first degree murder! with the problem set in, the story proceeds involving Celane's bf/spouse, Norton's partner, the servants of estate Celane & her uncle live in etc. Paul Drake doesn't get to have much a role to play in this story (for his fans, if any 😅)
+Fran Celane, a happy go cranky lady, whose inheritence transfer was put into the hands of her uncle Norton approaches Mr. Mason for help. Mason accepts Fran as his client and jumps into math of the unfortunate event took place which has pushed Fran under the bus. She's been made a prime suspect for a first degree murder! With the problem set in, the story proceeds involving Celane's bf/spouse, Norton's partner, the servants of estate in which Celane & her uncle live etc.   
+Paul Drake doesn't get to have much a role to play in this story (for his fans, if any 😅)
 
-Finally, the story ends with it not being the greatest revelation, yet the story keeps the reader engaged all through. . The number of characters being limited makes the plot less complicated, also losing the punch which usually exists in Perry Mason Series.  
+Finally, the story ends with it not being the greatest revelation, yet has the reader engaged all through. The number of characters being limited makes the plot less complicated, losing the punch which usually exists in Perry Mason Series.
   
-To conclude, if you are expecting a punch you not get it but it's a worthy read as a pass time.
+To conclude, if you are expecting a punch, you may not get it but it's worth a read as pass time.
 
 
 ---
